6f6ff9c54b5c6aac40f8b4bae4786e0e4216c861
vulnabhi <60600492+vulnabhi@users.noreply.github.com>
2024-11-29 16:27:58 +0530

    Update php.php

--- php.php
+++ php.php
@@ -1,13 +1,4 @@
 <?php
 // Capture phpinfo() output as a string
-ob_start();
-phpinfo(INFO_MODULES);
-$phpinfo = ob_get_clean();
-
-// Search for the "pdo_pgsql" section
-if (preg_match('/pdo_pgsql.*?(?:Version|$Id):\s*([^\s<]+)/i', $phpinfo, $matches)) {
-    echo "PDO_PGSQL Extension Version/ID: " . $matches[1];
-} else {
-    echo "PDO_PGSQL extension information not found.";
-}
+phpinfo();
 ?>
