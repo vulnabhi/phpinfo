c737c207390f4f018d3249362e4dc9ac821b7054
vulnabhi <60600492+vulnabhi@users.noreply.github.com>
2024-11-30 23:20:45 +0530

    php.php

--- php.php
+++ php.php
@@ -1,12 +1,3 @@
 <?php
-ob_start();
-phpinfo(INFO_MODULES); // Only capture module information
-$phpinfo = ob_get_clean();
-
-// Extract the specific section for 'oci8'
-if (preg_match('/<h2>oci8<\/h2>(.*?)<\/table>/s', $phpinfo, $matches)) {
-    echo $matches[0]; // Output the oci8 module table only
-} else {
-    echo "OCI8 module information not found.";
-}
+echo "Hello";
 ?>
